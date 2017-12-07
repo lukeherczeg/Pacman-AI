b84d53bbfc35786bcb7e294080a40a8be5c696f8
lukeherczeg <lukeherczeg@ufl.edu>
2017-12-06 22:14:46 -0500

    Small changes to make it perfecto

--- src/ufl/cs1/controllers/StudentController.java
+++ src/ufl/cs1/controllers/StudentController.java
@@ -11,17 +11,12 @@ import java.util.List;
 public final class StudentController implements DefenderController
 {
 	// Up - Right - Down - Left -> 0 - 1 - 2 - 3
-	// Blinky - Pinky - Clyde - Inky -> 0 - 1 - 2 - 3
+	// Blinky(Red) - Pinky(Pink) - Clyde(Orange) - Inky(Blue) -> 0 - 1 - 2 - 3
 
 	private Game currentGameState;
 	private Game previousGameState;
 
 	private final int noAction = -1;
-	int up = 0;
-    int right = 1;
-    int down = 2;
-    int left = 3;
-
 	private final int Blinky = 0;
 	private final int Pinky = 1;
 	private final int Clyde = 2;
@@ -35,7 +30,7 @@ public final class StudentController implements DefenderController
 	{
 		this.currentGameState = game;
 		if (this.previousGameState == null){
-			this.previousGameState = this.currentGameState;
+			this.previousGameState = game;
 		}
 		int[] actions = new int[Game.NUM_DEFENDER];
 
@@ -43,19 +38,15 @@ public final class StudentController implements DefenderController
 		actions[Pinky] = ghostAction(Pinky);
 		actions[Clyde] = ghostAction(Clyde);
 		actions[Inky] = ghostAction(Inky);
-		this.previousGameState = this.currentGameState;
+
+		this.previousGameState = game;
 		return actions;
 	}
-	//methods for each individual ghost, one can be a repeat because we are three people and not four.
-	//we have to make each one different, but i made all the same to start just becacuse
+	//each individual ghost, one can be a repeat because we are three people and not four.
+	//Variations can be found in the chase method, where we use if statements for each ghost's id
 
 	private int ghostAction(int ghostID){
-		if (StudentController.this.currentGameState.getDefender(ghostID).isVulnerable()){
-			return frightened(ghostID);
-		}
-		else{
-			return chase(ghostID);
-		}
+		return StudentController.this.currentGameState.getDefender(ghostID).isVulnerable() ? frightened(ghostID) : chase(ghostID);
 	}
 
 	//frightened method for when powerpill is eaten. Once ghosts get far enough away, they will come back since they don't want to be too far to hunt him when the frightened state ends
@@ -63,15 +54,13 @@ public final class StudentController implements DefenderController
 		Node pacmanLocation = StudentController.this.currentGameState.getAttacker().getLocation();
 		Defender ghost = StudentController.this.currentGameState.getDefender(ghostId);
 
-		if(pacmanLocation.isPowerPill() || ghost.getVulnerableTime() > 0)
-			if (pacmanLocation.getPathDistance(ghost.getLocation()) < 30)  // tested between 0-100
-				return ghost.getNextDir(pacmanLocation, false); // some movement away from pacman\
-			else
-				return ghost.getNextDir(pacmanLocation, true);
+		if(pacmanLocation.isPowerPill() || ghost.getVulnerableTime() > 0) // ternary operator for movement away from pacman if close, towards if far
+			return (pacmanLocation.getPathDistance(ghost.getLocation()) < 30) ? (ghost.getNextDir(pacmanLocation, false)) : (ghost.getNextDir(pacmanLocation, true));
+
 		return noAction;
 	}
 
-	//chase method
+	//chase method for moving towards pacman
 	private int chase(int ghostId) {
 		int direction = noAction;
 		Defender ghost = StudentController.this.currentGameState.getDefender(ghostId);
@@ -84,23 +73,21 @@ public final class StudentController implements DefenderController
 
 		if ((ghostId == Blinky || ghostId == Pinky) && pacman.getPossibleDirs(false).contains(pacDir)) {
 			direction = ghost.getNextDir(pacmanLocation.getNeighbor(pacDir), true);
-		}                 // aims in front of pacman
+		}                      // aims in front of pacman
 
-		if (ghostId == Clyde)   // aims behind pacman
+		if (ghostId == Clyde)  // aims behind pacman
 			direction = ghost.getNextDir(pacmanLocationLast, true);
 
 		if (ghostId == Inky)  // aim straight at pacman
 			direction = ghost.getNextDir(pacmanLocation, true);
 
-		if (ghostId == Blinky || ghostId == Pinky || ghostId == Clyde) {    // makes all ghosts but 1 flee when pacman is close to a power pill, and one goes towards him to force him to eat it.
+		if (ghostId == Blinky || ghostId == Pinky || ghostId == Clyde) {    // makes all ghosts but one flee when pacman is close to a power pill, and one goes towards him to force him to eat it.
 			for (int i = 0; i < powerUps.size(); i++) {
 				if (pacmanLocation.getPathDistance(powerUps.get(i)) < 20) {
 					direction = ghost.getNextDir(pacmanLocation, false);
 				}
 			}
 		}
-
 		return direction;
 	}
-
 }
